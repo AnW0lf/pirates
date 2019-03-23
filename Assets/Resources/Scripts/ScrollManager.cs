357ba6fe9097c91b17b1c53204e362d89788fd72
SergeyVolkov <sergeyvolkovapplife@users.noreply.github.com>
2019-03-23 15:28:57 +0300

    Scale by width
    Автоскейл

--- Assets/Resources/Scripts/ScrollManager.cs
+++ Assets/Resources/Scripts/ScrollManager.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class ScrollManager : MonoBehaviour
 {
     public int level;
+    public Canvas game;
     private RectTransform rect;
     private Island island;
-    private float unit = 1920f;
+    private float unit = 2436f;
     private int childCount;
 
     private void Awake()
@@ -23,5 +24,10 @@ public class ScrollManager : MonoBehaviour
         Vector2 pos = new Vector3(rect.localPosition.x, sizeY - (unit * (1 + island.Level / level)), rect.localPosition.z);
         rect.sizeDelta = new Vector2(.0f, sizeY);
         rect.localPosition = pos;
+
+        foreach (RectTransform child in rect)
+        {
+            child.sizeDelta = new Vector2(game.GetComponent<RectTransform>().sizeDelta.x, child.sizeDelta.y);
+        }
     }
 }
