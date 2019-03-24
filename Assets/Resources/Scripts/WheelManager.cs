74c6c9bf4c540dbdb852956f84279ca588d91a35
DESKTOP-R0VLOMB\Egor <AngryWolfgang@gmail.com>
2019-03-24 22:27:46 +0300

    Игра - Колесо
    Сделан выезд колеса, но пока оно не подстраивается по высоте.

--- Assets/Resources/Scripts/WheelManager.cs
+++ Assets/Resources/Scripts/WheelManager.cs
@@ -6,18 +6,38 @@ using UnityEngine.UI;
 public class WheelManager : MonoBehaviour
 {
     public Text wheelCounter;
-
+    public float posOn, posOff;
     public static int wheelTokens;
+
+    public bool IsOpen { get; private set; }
+    private Transform parent;
+
+    private void Awake()
+    {
+        parent = transform.parent;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         wheelTokens = 0;
+        IsOpen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         wheelCounter.text = "Lifebuoys: " + wheelTokens + "/" + PlayerPrefs.GetFloat("GlobalSpins");
+        //parent.Translate(Vector3.forward * (parent.position.y - (IsOpen ? posOn: posOff)) * Time.deltaTime);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        IsOpen = !IsOpen;
+    }
+
+    private void OnMouseDrag()
+    {
+        parent.position = Input.mousePosition;
     }
 }
