ad09095e0155a99a86b8770658718bdec29cc878
DESKTOP-R0VLOMB\Egor <AngryWolfgang@gmail.com>
2019-03-27 23:39:08 +0300

    Игра - Остров
    По нажати. на остров все вернувшиеся и возвращающиеся корабли отправляются в плавание.

---
+++ Assets/Resources/Scripts/IslandController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandController : MonoBehaviour
+{
+    public Transform ships;
+
+    public void BeginRaids()
+    {
+        foreach(Ship ship in ships.GetComponentsInChildren<Ship>())
+        {
+            ship.BeginRaid();
+        }
+    }
+}
