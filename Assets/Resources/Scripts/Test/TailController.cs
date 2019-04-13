76735fe53250355092bb2d9a402e54c38433e3d8
DESKTOP-R0VLOMB\Egor <AngryWolfgang@gmail.com>
2019-04-13 23:02:18 +0300

    След корабля ТЕСТ
    Хвосты не отключаются, а их таймеру присваивается значение 0.

---
+++ Assets/Resources/Scripts/Test/TailController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailController : MonoBehaviour
+{
+    private TrailRenderer tr;
+    private Transform content;
+
+    UnityEngine.UI.ScrollRect sr;
+
+    Vector3 pos;
+
+    private void Awake()
+    {
+        tr = GetComponent<TrailRenderer>();
+        content = GameObject.FindGameObjectWithTag("EditorOnly").GetComponent<UnityEngine.UI.ScrollRect>().content;
+    }
+
+    private void FixedUpdate()
+    {
+        if (content.position == pos) tr.time = 0.75f;
+        if (content.position != pos) tr.time = 0f;
+        pos = content.position;
+    }
+
+}
