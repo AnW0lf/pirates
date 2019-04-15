b93c3ddf8511cbe501d27e9500cb783c10e285df
SergeyVolkov <sergeyvolkovapplife@users.noreply.github.com>
2019-04-15 16:19:48 +0300

    some fixes

--- Assets/Resources/Scripts/BonusBehavior.cs
+++ Assets/Resources/Scripts/BonusBehavior.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BonusBehavior : MonoBehaviour
 {
     public bool bonusMoney, bonusSpeed, bonusWheel;
     public int modifier;
+
+    public string textOnClick;
+    public GameObject textOnClickObject;
+
+    private GameObject _textOnClick;
+
+    private void OnMouseUp()
+    {
+        _textOnClick = Instantiate(textOnClickObject, gameObject.transform);
+        _textOnClick.GetComponent<Text>().text = textOnClick;
+        _textOnClick.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+    }
 
 }
