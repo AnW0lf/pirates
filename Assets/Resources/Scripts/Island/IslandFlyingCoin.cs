726a2209160bf04de90444ca5646692c682ede91
SergeyVolkov <sergeyvolkovapplife@users.noreply.github.com>
2019-06-24 19:57:35 +0300

    Revert "Revert "Оптимизация""
    This reverts commit e2d060dc894be73adb164f8ac48a068c5a734a82.

---
+++ Assets/Resources/Scripts/Island/IslandFlyingCoin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IslandFlyingCoin : MonoBehaviour
+{
+    [SerializeField]
+    private Text count;
+    private Animator anim;
+    private bool hasText = false;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        if (count != null || count == null && transform.childCount > 0 && (count = transform.GetChild(0).GetComponent<Text>()) != null)
+            hasText = true;
+    }
+
+    public void Fly(BigDigit value)
+    {
+        if (!hasText) return;
+        count.text = "+" + value.ToString();
+        anim.SetTrigger("Fly");
+    }
+}
