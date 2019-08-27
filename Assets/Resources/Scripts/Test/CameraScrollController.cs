a71104375e74c0986458ac58477e9cd79a71f689
AnW0lf <AngryWolfgang@gmail.com>
2019-08-27 13:42:08 +0300

    CameraScrollController
    Removed: tail controllers

---
+++ Assets/Resources/Scripts/Test/CameraScrollController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScrollController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform content = null;
+    [SerializeField]
+    private CanvasScaler canvas = null;
+
+    private Camera mainCamera;
+    private Transform cameraTransform;
+    private float k;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        cameraTransform = mainCamera.transform;
+        if (content == null) Debug.LogWarning("Content is not defined");
+        if (canvas == null) Debug.LogWarning("Canvas is not defined");
+    }
+
+    private void Start()
+    {
+        k = (float)canvas.referenceResolution.y / (mainCamera.orthographicSize * 2f);
+        print(k);
+        print(content.position.y);
+    }
+
+    private void Update()
+    {
+        float newY = -(content.localPosition.y / k);
+        print(newY);
+        cameraTransform.position = new Vector3(cameraTransform.position.x, newY, cameraTransform.position.z);
+    }
+}
