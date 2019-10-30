5a61ac9fd7c9e2afaf6804f0646124877d1dec9b
AnW0lf <AngryWolfgang@gmail.com>
2019-10-31 02:01:47 +0300

    GameAnalytics инициализация

---
+++ Assets/GA_Initialize.cs
@@ -0,0 +1,16 @@
+﻿using GameAnalyticsSDK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GA_Initialize : MonoBehaviour
+{
+
+#if UNITY_IPHONE
+    void Start()
+    {
+        GameAnalytics.Initialize();
+    }
+#endif
+
+}
