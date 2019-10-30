1f089e1e73dfb2a5a1f1942007653193959d12b4
AnW0lf <AngryWolfgang@gmail.com>
2019-10-31 01:46:43 +0300

    Cheat

---
+++ Assets/Cheat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheat : MonoBehaviour
+{
+    public void LevelUp()
+    {
+        Island.Instance.LevelUp();
+    }
+
+    public void AddMoney(int exponent)
+    {
+        Island.Instance.ChangeMoney(new BigDigit(1, exponent));
+    }
+
+    public void AddPremium(int exponent)
+    {
+        Island.Instance.ChangePremium(new BigDigit(1, exponent));
+    }
+
+    public void AddSpin(int a)
+    {
+        Island.Instance.ChangeLifebuoy(a);
+    }
+
+    public void Resetting()
+    {
+        Island.Instance.Resetting();
+    }
+}
