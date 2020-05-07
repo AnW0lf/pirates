54fca03a753684554f8a97ee4fd432ad5e40e57a
AnW0lf <AngryWolfgang@gmail.com>
2020-05-07 15:52:58 +0300

    Промежуточный 2D

--- Assets/Resources/Scripts/2DEdition/ProgressBar.cs
+++ Assets/Resources/Scripts/2DEdition/ProgressBar.cs
@@ -1,27 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
-    public bool Visible { get => _visible; set
+    public bool Visible
+    {
+        get => _visible; set
         {
-            _visible = value;
-            progressBar.SetActive(_visible);
+            if (value != _visible)
+            {
+                _visible = value;
+                if (_visible)
+                {
+                    progress.gameObject.LeanAlpha(1f, showDuration);
+                    label.gameObject.LeanAlpha(1f, showDuration);
+                    icon.gameObject.LeanAlpha(1f, showDuration);
+                }
+                else
+                {
+                    progress.gameObject.LeanAlpha(0f, hideDuration);
+                    label.gameObject.LeanAlpha(0f, hideDuration);
+                    icon.gameObject.LeanAlpha(0f, hideDuration);
+                }
+            }
+
         }
     }
 
     [SerializeField]
     private bool _visible = true;
     [SerializeField]
-    private GameObject progressBar = null;
+    private SpriteRenderer icon = null;
     [SerializeField]
-    private Image progress = null;
+    private Transform progress = null;
     [SerializeField]
-    private Text label = null;
+    private TextMesh label = null;
+    [SerializeField]
+    private float showDuration = 0.5f, hideDuration = 0.5f;
 
-    public float Progress { get => progress.fillAmount; set => progress.fillAmount = value; }
+    public float Progress { get => progress.localScale.x; set => progress.localScale = new Vector3(Mathf.Clamp01(value), progress.localScale.y, progress.localScale.z); }
 
     public string Label { get => label.text; set => label.text = value; }
+
+    private void Start()
+    {
+        if (_visible)
+        {
+            progress.gameObject.LeanAlpha(1f, 0f);
+            label.gameObject.LeanAlpha(1f, 0f);
+            icon.gameObject.LeanAlpha(1f, 0f);
+        }
+        else
+        {
+            progress.gameObject.LeanAlpha(0f, 0f);
+            label.gameObject.LeanAlpha(0f, 0f);
+            icon.gameObject.LeanAlpha(0f, 0f);
+        }
+    }
 }
