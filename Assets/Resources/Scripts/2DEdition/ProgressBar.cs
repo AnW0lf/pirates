196f6b393a256e16449c8e3fe4a3a768f7fe4614
AnW0lf <AngryWolfgang@gmail.com>
2020-05-09 00:54:57 +0300

    Revert "Промежуточный 2D"
    This reverts commit 54fca03a753684554f8a97ee4fd432ad5e40e57a.

--- Assets/Resources/Scripts/2DEdition/ProgressBar.cs
+++ Assets/Resources/Scripts/2DEdition/ProgressBar.cs
@@ -1,61 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
-    public bool Visible
-    {
-        get => _visible; set
+    public bool Visible { get => _visible; set
         {
-            if (value != _visible)
-            {
-                _visible = value;
-                if (_visible)
-                {
-                    progress.gameObject.LeanAlpha(1f, showDuration);
-                    label.gameObject.LeanAlpha(1f, showDuration);
-                    icon.gameObject.LeanAlpha(1f, showDuration);
-                }
-                else
-                {
-                    progress.gameObject.LeanAlpha(0f, hideDuration);
-                    label.gameObject.LeanAlpha(0f, hideDuration);
-                    icon.gameObject.LeanAlpha(0f, hideDuration);
-                }
-            }
-
+            _visible = value;
+            progressBar.SetActive(_visible);
         }
     }
 
     [SerializeField]
     private bool _visible = true;
     [SerializeField]
-    private SpriteRenderer icon = null;
+    private GameObject progressBar = null;
     [SerializeField]
-    private Transform progress = null;
+    private Image progress = null;
     [SerializeField]
-    private TextMesh label = null;
-    [SerializeField]
-    private float showDuration = 0.5f, hideDuration = 0.5f;
+    private Text label = null;
 
-    public float Progress { get => progress.localScale.x; set => progress.localScale = new Vector3(Mathf.Clamp01(value), progress.localScale.y, progress.localScale.z); }
+    public float Progress { get => progress.fillAmount; set => progress.fillAmount = value; }
 
     public string Label { get => label.text; set => label.text = value; }
-
-    private void Start()
-    {
-        if (_visible)
-        {
-            progress.gameObject.LeanAlpha(1f, 0f);
-            label.gameObject.LeanAlpha(1f, 0f);
-            icon.gameObject.LeanAlpha(1f, 0f);
-        }
-        else
-        {
-            progress.gameObject.LeanAlpha(0f, 0f);
-            label.gameObject.LeanAlpha(0f, 0f);
-            icon.gameObject.LeanAlpha(0f, 0f);
-        }
-    }
 }
