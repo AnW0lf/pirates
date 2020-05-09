291d2af8508e703655f49628a1b7b8d1621b972c
SergeyVolkov <sergeyvolkovapplife@users.noreply.github.com>
2020-05-09 04:06:41 +0300

    new arrow

---
+++ Assets/VerticalGlobalPos.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalGlobalPos : MonoBehaviour
+{
+
+    void Update()
+    {
+        gameObject.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        //if (gameObject.transform.parent.transform.parent.transform.eulerAngles.z > 100f)
+        //{
+        //    gameObject.transform.eulerAngles += new Vector3(0f, 0f, 180f);
+        //}
+    }
+}
