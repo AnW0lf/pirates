74ef2555ecd5e26df3214257026394c891a3af18
AnW0lf <AngryWolfgang@gmail.com>
2020-06-22 16:03:10 +0300

    Quest panel active

--- Assets/Resources/Scripts/Interface/PanelQuests.cs
+++ Assets/Resources/Scripts/Interface/PanelQuests.cs
@@ -11,64 +11,73 @@ public class PanelQuests : MonoBehaviour
     [SerializeField] private Text lvlText = null, questText = null, rankText = null;
     [SerializeField] public bool opened = false;
     [SerializeField] private List<int> levels = null;
-    [SerializeField] private List<string> texts = null;
+    [SerializeField] private string textPattern = null;
 
     private Island island;
     private int questLevel = 0;
 
     public List<int> Levels { get => levels; }
 
-    //private void Awake()
-    //{
-    //    island = Island.Instance();
-    //}
+    private void Awake()
+    {
+        island = Island.Instance();
+    }
 
-    //private void Start()
-    //{
-    //    island.InitParameter("QuestLevel", 0);
-    //    questLevel = island.GetParameter("QuestLevel", 0);
-    //    UpdateInfo();
+    private void Start()
+    {
+        island.InitParameter("QuestLevel", 0);
+        questLevel = island.GetParameter("QuestLevel", 0);
+        UpdateInfo();
 
-    //    island.InitParameter("QuestOpened", 0);
-    //    opened = island.GetParameter("QuestOpened", 0) != 0;
-    //    if (opened)
-    //        transform.localPosition += Vector3.down * 200f;
-    //    else
-    //        StartCoroutine(Open());
-    //}
+        island.InitParameter("QuestOpened", 0);
+        opened = island.GetParameter("QuestOpened", 0) != 0;
+        if (opened)
+            GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        else
+            StartCoroutine(Open());
+    }
 
-    //public void UpdateInfo()
-    //{
-    //    if (questLevel >= texts.Count || questLevel >= levels.Count) return;
-    //    progressBar.fillAmount = (float)island.Level / levels[questLevel];
-    //    lvlText.text = island.Level + "/" + levels[questLevel];
-    //    questText.text = texts[questLevel];
-    //    rankText.text = (questLevel + 1).ToString();
-    //    if (island.Level >= levels[questLevel])
-    //    {
-    //        button.SetActive(true);
-    //    }
-    //}
+    public void UpdateInfo()
+    {
+        if (questLevel >= levels.Count) return;
+        progressBar.fillAmount = (float)island.Level / levels[questLevel];
+        lvlText.text = island.Level + "/" + levels[questLevel];
+        questText.text = string.Format(textPattern, levels[questLevel]);
+        rankText.text = (questLevel + 1).ToString();
+        if (island.Level >= levels[questLevel])
+        {
+            button.SetActive(true);
+        }
+    }
 
-    //public void QuestDone()
-    //{
-    //    questLevel++;
-    //    island.SetParameter("QuestLevel", questLevel);
-    //    button.SetActive(false);
-    //    UpdateInfo();
-    //}
+    public void QuestDone()
+    {
+        questLevel++;
+        island.SetParameter("QuestLevel", questLevel);
+        button.SetActive(false);
+        UpdateInfo();
+    }
 
-    //private IEnumerator Open()
-    //{
-    //    bool Opened() { return opened; }
-    //    yield return new WaitUntil(Opened);
-    //    RectTransform rect = GetComponent<RectTransform>();
-    //    WaitForSeconds wait = new WaitForSeconds(0.025f);
-    //    for (int i = 0; i < 20; i++)
-    //    {
-    //        rect.localPosition += Vector3.down * 10f;
-    //        yield return wait;
-    //    }
-    //    island.SetParameter("QuestOpened", 1);
-    //}
+    private IEnumerator Open()
+    {
+        RectTransform rect = GetComponent<RectTransform>();
+        WaitUntil waitUntil = new WaitUntil(() => { return opened; });
+
+        rect.anchoredPosition = Vector2.up * 200f;
+
+        yield return waitUntil;
+
+        float timer = 0f;
+        Vector2 start = rect.anchoredPosition;
+        Vector2 end = Vector2.zero;
+
+        while(timer < 0.5f)
+        {
+            timer += Time.deltaTime;
+            rect.anchoredPosition = Vector2.Lerp(start, end, timer / 0.5f);
+            yield return null;
+        }
+
+        island.SetParameter("QuestOpened", 1);
+    }
 }
